90a18a4efb46ea390d7a4a8cbac8b49c6db4bd37
gseelam48 <gseelam48@gmail.com>
2025-06-03 22:17:07 -0500

    deleted sample.java

--- sample.java
+++
@@ -1,8 +0,0 @@
-public class sample {
-    public static void main(String[] args){
-        int i = 2;
-        int x = 3;
-        int result = i+x;
-        System.out.println("result:" + result);
-    }
-}
